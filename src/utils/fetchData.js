48e658a562c1a03a6a461b0161bb0e4131692603
SaiGodaUI <saigoda2@gmail.com>
2023-05-04 17:08:38 -0500

    removing secret key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,19 @@
+import { seretKey } from "./secretKey";
+
 export const exerciseOptions = {
   method: "GET",
-  //   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    "X-RapidAPI-Key": "2aeceb9af4msh1b6e58812a1f885p1fc248jsne3c43908343a",
+    "X-RapidAPI-Key": seretKey,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
 
 export const youtubeOptions = {
-  method: 'GET',
-  // url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Key': '2aeceb9af4msh1b6e58812a1f885p1fc248jsne3c43908343a',
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": seretKey,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
 };
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
